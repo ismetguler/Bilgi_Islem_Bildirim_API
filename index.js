400a1d580c9f645e2a873aaddf5082fa005412a8
İsmet Güler <gulerrismet@gmail.com>
2026-02-26 16:58:09 +0300

    feat: proje iskeleti olusturuldu ve express sunucusu ayaga kaldirildi

---
+++ index.js
@@ -0,0 +1,16 @@
+// Express kütüphanesini projemize dahil ediyoruz
+const express = require('express'); 
+const app = express();
+
+// Sunucunun çalışacağı port numarasını belirliyoruz
+const PORT = 3000;
+
+// Ana sayfaya (/) bir istek geldiğinde verilecek cevap
+app.get('/', (req, res) => {
+    res.send('Bilgi Islem Bildirim API basariyla calisiyor!');
+});
+
+// Sunucuyu ayaklandırıp dinlemeye başlıyoruz
+app.listen(PORT, () => {
+    console.log(`Sunucu http://localhost:${PORT} adresinde ayaklandi!`);
+});
